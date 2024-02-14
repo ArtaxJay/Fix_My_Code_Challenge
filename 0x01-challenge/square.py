20a72451059cc0eab76a9aff5cd93667c3354e2f
ArtaxJay <okjaceauthor@gmail.com>
2024-02-14 03:51:02 +0000

    Modi square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,7 +3,7 @@
 # ArtaxJay Olaoluwa
 # as an ALX SE Project
 
-class square():
+class Square:
     
     width = 0
     height = 0
@@ -17,15 +17,18 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        Returns a str representation of the square class.
+        """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
